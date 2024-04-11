f1621e76d4ec38b859b420f169e4164c70e90e1d
Akshar2325 <aksharbhesaniya@gmail.com>
2024-04-11 07:23:52 +0530

    added navbar,Home,Some expolre menu pages

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="header-content">
+                <h2>Order Your favourite food here</h2>
+
+                <p>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingrediets and culinary expertise.Our mission is to  satisfy your craving 7 elevate your dinner experince, one delicious meal at a time.</p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
